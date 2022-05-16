f8ca2d7ad8b694e6e6f24141035eeabf315b55f3
RhamzThev <rhamzthev@gmail.com>
2022-05-16 16:00:28 -0400

    creating application example to set up communication

---
+++ myPortfolio/Apps/Example/controller.py
@@ -0,0 +1,10 @@
+class Controller:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+
+    def win():
+        raise NotImplementedError
+
+    def lose():
+        raise NotImplementedError
