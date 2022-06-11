565c40d576d491038846a6c06cbd1a6d3f42dd1d
RhamzThev <rhamzthev@gmail.com>
2022-06-11 15:11:22 -0400

    added comments to miscellaneous class.

--- myPortfolio/Services/Miscellaneous.cs
+++ myPortfolio/Services/Miscellaneous.cs
@@ -6,8 +6,16 @@ using System.Windows.Media;
 
 namespace myPortfolio.Services
 {
+    /// <summary>
+    /// A class where miscellaneous helper functions will be contained.
+    /// </summary>
     public class Miscellaneous
     {
+        /// <summary>
+        /// Changes the password box's background text.
+        /// </summary>
+        /// <param name="sender">The sender object (supposed to be <c>PasswordBox</c>)</param>
+        /// <param name="text">What the background text will be changed to.</param>
         public static void ChangePasswordBox(object sender, string text)
         {
             VisualBrush visualBrush = SetBlock(text);
@@ -16,6 +24,11 @@ namespace myPortfolio.Services
             ((PasswordBox)sender).Background = visualBrush;
         }
 
+        /// <summary>
+        /// Changes the text box's background text.
+        /// </summary>
+        /// <param name="sender">The sender object (supposed to be <c>TextBox</c>)</param>
+        /// <param name="text">What the background text will be changed to.</param>
         public static void ChangeTextBox(object sender, string text)
         {
             VisualBrush visualBrush = SetBlock(text);
@@ -24,6 +37,11 @@ namespace myPortfolio.Services
             ((TextBox)sender).Background = visualBrush;
         }
 
+        /// <summary>
+        /// Set's background text to given text.
+        /// </summary>
+        /// <param name="text">The given text</param>
+        /// <returns>The new background as a <c>VisualBrush</c>.</returns>
         private static VisualBrush SetBlock(string text)
         {
             // CREATE VISUAL BRUSH
